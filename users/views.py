7d81028fe703d53f0ecc02c15d9c79eaea13627b
Tim Lin <timedu12@gmail.com>
2024-01-26 13:28:05 -0500

    reformat files

--- users/views.py
+++ users/views.py
@@ -9,10 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-        # else:
-        #     messages.error(request, f'form error')
+            messages.success(request, f'Your account has been created! You are now able to login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})

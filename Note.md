61654b801fd44721fcf7a8ca8d64cddcf05c2c2d
Tim Lin <timedu12@gmail.com>
2024-01-26 11:07:49 -0500

    register

--- Note.md
+++ Note.md
@@ -1,5 +1,4 @@
-ar
-e.\pyenv\Scripts\activate
+.\pyenv\Scripts\activate
 deactivate
 
 
@@ -10,3 +9,22 @@ django-admin
 
 start server
     python .\manage.py runserver
+
+
+    python .\manage.py createsuperuser
+        tim - admin
+        test112122312@gmail.com
+        password1234
+
+        testuser - Supper11
+    python .\manage.py makemigrations
+
+
+
+user.post_set.create(title='Blog3', content='Third Post Content')
+
+
+>>> user = User.objects.first()
+>>>
+>>> post_2 = Post(title='Blog2', content='Second Post Content', author=user)
+>>> post_2.save()
---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import  UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+        # else:
+        #     messages.error(request, f'form error')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+"""
+messages.debug
+messages.info
+messages.success
+messages.warning
+messages.error
+"""
